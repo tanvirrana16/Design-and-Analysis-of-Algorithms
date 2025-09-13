1805642de75af039ec078b7fdc59bb4f157be58f
Tanvir Rana <tanvirrana1146@gmail.com>
2025-09-13 20:29:13 +0600

    Assignment 3

--- Quicksort.c
+++ Quicksort.c
@@ -1,31 +1,32 @@
 #include<stdio.h>
-void swap(int* a,int* b){
-    int temp=*a;
-    *a=*b;
-    *b=temp;
+void swap(int* a,int* b){// swapping the two variable
+    int temp=*a; //take a temp variable which store the Address of a
+    *a=*b;// copy the address of b at a
+    *b=temp; // then declare temp as a b which was before declared the address of a
 }
-int partition(int a[],int lb,int ub){
-    int pivot=a[lb];
-    int start=lb;
-    int end=ub;
-    while(start<end){
-        while(a[start]<=pivot){
-            start++;
+int partition(int a[],int lb,int ub){ //partition function
+    int pivot=a[lb]; //take a pivot value at lower bound
+    int start=lb; //initialize variable start as lower bound
+    int end=ub;//initialize variable end as upper bound
+    while(start<end){ //check  start less than end in while loop
+        while(a[start]<=pivot){// check value of a[start] less than or equal to pivot 
+            start++;// then increment start
         }
-        while(a[end]>pivot){
-            end--;
-        }if(start<end){
-        swap(&a[start],&a[end]);
+        while(a[end]>pivot){//check value of a[end] greater than pivot 
+            end--; //then decrement end =end-1
+        }
+        if(start<end){  // check if start is less than end
+        swap(&a[start],&a[end]);  //then exchange  a[start] with a[end]
     }
 }
-    swap(&a[lb],&a[end]);
-    return end;
+    swap(&a[lb],&a[end]);//if not then exchange a[lb] with a[end]
+    return end;//at last return the index of end
 }
-void Quicksort(int a[],int lb,int ub){
-     if(lb<ub){
-      int loc=partition(a,lb,ub);
-      Quicksort(a,lb,loc-1);
-      Quicksort(a,loc+1,ub);
+void Quicksort(int a[],int lb,int ub){  //
+     if(lb<ub){//check if lower bound less than upper bound
+      int loc=partition(a,lb,ub); //declare a location varibale in the return value from the partition function 
+      Quicksort(a,lb,loc-1);//then again call recursively from the lower bound to location-1 
+      Quicksort(a,loc+1,ub);//then again call recursively from the location+1 to ubber bound
      }
 }
 int main(){
